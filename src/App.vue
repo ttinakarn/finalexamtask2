bd88970c16e7af48a6e31f2b3ff111f2aac5fd08
tinakarn <haiiplara@hotmail.com>
2018-12-15 16:14:04 +0700

    Add search button and npm install vue-chartjs chart.js --save

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
   <div id="app">
     <b-navbar toggleable="md" type="dark" variant="info">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand>Final Exam</b-navbar-brand>
+      <b-navbar-brand>FinalExam</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
           <b-nav-item href="https://final-exam-task1.herokuapp.com/api/products" class="nav-link">Task 1</b-nav-item>
-          <b-nav-item>
-            <router-link to="/task2" class="nav-link">Task 2</router-link>
+          <b-nav-item class="nav-link">
+            <router-link to="/task2"></router-link>Task 2
           </b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar><br>
